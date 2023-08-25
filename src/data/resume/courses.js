14a8a1fdbed7fd18f44059151aae17db94f61c4b
Devanshu-1510 <devanshu.saini20@st.niituniversity.in>
2023-08-25 14:53:07 +0530

    main

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -8,8 +8,20 @@ const courses = [
   {
     title: 'Front-End Software Engineering Virtual Experience Programme at Skyscanner',
     number: '2',
-    link: 'https://docs.google.com/document/d/1ptv2177VrRKX6gvyFzRY3iB4oHREUYqdKymb6BuAoTY/edit?usp=sharing',
+    link: 'https://docs.google.com/document/d/1HOC8srUli8wA3QjZ6lRYGG8c0QcAlstCo9pJjL9Py1s/edit?usp=sharing',
     university: 'Stanford',
+  },
+  {
+    title: 'Front-End Intern Lead',
+    number: '3',
+    link: 'https://docs.google.com/document/d/1qSjE7tg9eFudHE7dB6na-WpajLC9KfTEc1o3VmTHpHs/edit?usp=sharing',
+    university: 'Capitalist Yard India',
+  },
+  {
+    title: 'siNUsoid Core Organiser',
+    number: '3',
+    link: 'https://docs.google.com/document/d/1tdkhuZi7v3KJnBdtc1f8ENWMGVhK2zKQyrbUtDn9C6I/edit?usp=sharing',
+    university: 'NIIT University',
   },
 ];
 
